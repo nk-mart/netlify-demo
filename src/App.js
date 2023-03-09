baf2b21b6114a9d6ec384e97fe504056098c8a01
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-03-09 18:53:28 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Edx net app</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <h1>Edx net app</h1>
+      
     </div>
   );
 }
